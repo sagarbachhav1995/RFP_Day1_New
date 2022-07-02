dcd70041a72b67a518239da12613eb11395e0f3f
sagarbachhav1995 <107263157+sagarbachhav1995@users.noreply.github.com>
2022-07-02 21:04:41 +0530

    Add files via upload

--- Prob_04.java
+++ Prob_04.java
@@ -4,12 +4,6 @@ public class Prob_04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter a command line" );
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
-	
-		
-		System.out.println("You entered " +name);
 
 	}
 
